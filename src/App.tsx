4e89e861fbcc6e8c8b556e46f78354c0a302be78
Dev-BrunoGSilva <134289002+Dev-BrunoGSilva@users.noreply.github.com>
2024-12-03 20:58:47 -0300

    Update App.tsx
    Trocar cor

--- src/App.tsx
+++ src/App.tsx
@@ -113,11 +113,11 @@ export default function App() {
       ) : (
         <Tabs>
           <TabList>
-            <Tab>Favoritos</Tab>
-            {Object.keys(eventsByDay).map((day) => (
-              <Tab key={day}>{day}</Tab>
-            ))}
-          </TabList>
+  <Tab className="tab-favoritos">Favoritos</Tab>
+  {Object.keys(eventsByDay).map((day, index) => (
+    <Tab key={day} className={`tab-dia-${index}`}>{day}</Tab>
+  ))}
+</TabList>
 
           <TabPanel>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
