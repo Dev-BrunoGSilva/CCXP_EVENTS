b1be0b5bf7065dcf41a5187f14ad4a692e07f5c6
Dev-BrunoGSilva <brunogomesdsilva7@gmail.com>
2024-12-04 13:41:02 -0300

    fix:direção

--- src/App.tsx
+++ src/App.tsx
@@ -107,7 +107,7 @@ export default function App() {
     : {};
 
   return (
-    <div className="min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 flex flex-col items-center justify-center p-4">
+    <div className="min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 flex items-center justify-center p-4">
       {loading ? (
         <h1 className="text-2xl text-gray-700">Carregando eventos...</h1>
       ) : (
